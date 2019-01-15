a8311197571d710fa0345e03e109aa2003333f80
Tanel Põld <tanel@brightside.ee>
2019-01-15 16:41:24 +0800

    9.5 Compatible now

--- Configuration/TypoScript/setup.ts
+++ Configuration/TypoScript/setup.ts
@@ -1,19 +1,19 @@
 plugin.tx_vimeovideo {
-	view < lib.fluidContent
+	view < lib.contentElement
     view.templateRootPaths.10 = EXT:vimeovideo/Resources/Private/Templates/
 	settings.defaultHeaderType = {$styles.content.defaultHeaderType}
-    
-    render {       
+
+    render {
        # wide screen 16:9 value (math behind: 9 * 100 / 16 = 56.25)
-        
+
         ratioDefault = TEXT
         ratioDefault.value = 56.25
 
        # screen 4:3 value (math behind: 3 * 100 / 4 = 75)
-        
+
         ratio1 = TEXT
         ratio1.value = 75
-                       
+
        # If you need more aspect ratios add em here and change Page TSConfig and HTML template accordingly.
 
         color= TEXT

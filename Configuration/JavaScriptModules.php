a0c01dc290bc2ada19cd1c85be5eb05fe41c21b9
Tanel Põld <tanel@brightside.ee>
2023-06-01 08:43:33 +0300

    [TASK] Clean Up

--- Configuration/JavaScriptModules.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-defined('TYPO3') || die ('Access denied.');
-
-return [
-    'imports' => [
-        '@t3brightside/vimeovideo/' => 'EXT:vimeovideo/Resources/Public/JavaScript/',
-    ],
-];
